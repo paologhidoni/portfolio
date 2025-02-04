bdf9b17f6de7d68998f48f5e2ff9662c3b9b09c3
paologhidoni <paolo.ghidoni82@gmail.com>
2025-02-04 22:51:14 +0100

    Cleanup unused code

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,6 @@ function App() {
       <main className="text-white font-quicksand">
         <Hero />
         <Skills />
-
-        <h1>PROJECTS</h1>
-        <p id="projects">
-          <br></br>
-          TEST! <br></br>
-          <br></br>
-          TESTINGTOOOOON! <br></br>
-          <br></br>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          deleniti sunt illo fugit, nostrum libero sed ipsa dolorem architecto?
-          Quis, facere delectus dolores rem animi rerum veritatis ipsum
-          reprehenderit quisquam. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Minus debitis id nemo molestiae quam nihil eos
-          dolores non, obcaecati aut repellat odit vel reprehenderit eveniet ea,
-          vero tempore, nostrum est. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Maxime velit vel sed praesentium, placeat ducimus
-          vero facilis eligendi assumenda id! Voluptatem reiciendis natus
-          assumenda architecto eveniet dolorem! Repudiandae, repellendus
-          numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Provident voluptas neque deserunt pariatur, vitae officia incidunt.
-          Officiis, aliquid nulla. Deleniti molestias ipsa fugiat omnis error
-          necessitatibus adipisci facere amet quibusdam!
-        </p>
       </main>
     </div>
   );
