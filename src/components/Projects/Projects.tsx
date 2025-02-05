b96ca4a82365910c5b8c4b6dac49767dc257fbdd
paologhidoni <paolo.ghidoni82@gmail.com>
2025-02-05 16:18:06 +0100

    Add Projects & Project components,render projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import Wrapper from "../../layouts/Wrapper";
 import Project from "./Project/Project";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import { projects } from "../../data/projects";
 
 const Projects: React.FC = () => {
   return (
-    <Wrapper id="projects" paddingY="py-6 md:py-10">
-      <Project
-        id="project-1"
-        title="Project 1"
-        description="Description 1"
-        image="image-1.jpg"
-        technologies={["React", "TailwindCSS", "TypeScript"]}
-        github="https://github.com"
-      />
-    </Wrapper>
+    <section id="projects" className="relative pb-12 bg-secondaryColor">
+      <SectionHeading title="Projects" />
+
+      <ul className="flex flex-col gap-8">
+        {projects.map((project, i) => (
+          <Wrapper
+            key={i + "_" + project.title}
+            skewedTop={true}
+            skewedEnd={true}
+            bgColor={i % 2 === 0 ? "bg-secondaryColor" : "bg-primaryColor"}
+          >
+            <Project
+              id={project.id}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              technologies={project.technologies}
+              github={project.github}
+              live={project.live}
+              i={i}
+            />
+          </Wrapper>
+        ))}
+      </ul>
+    </section>
   );
 };
 
