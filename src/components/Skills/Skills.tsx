ddb5f7f46321712e420709279b853f65121b8ffb
paologhidoni <paolo.ghidoni82@gmail.com>
2025-02-06 11:43:34 +0100

    Remove placeholder paragraph

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -111,22 +111,6 @@ const Skills: React.FC = () => {
             className="w-20 h-auto justify-self-center"
           />
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          deleniti sunt illo fugit, nostrum libero sed ipsa dolorem architecto?
-          Quis, facere delectus dolores rem animi rerum veritatis ipsum
-          reprehenderit quisquam. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Minus debitis id nemo molestiae quam nihil eos
-          dolores non, obcaecati aut repellat odit vel reprehenderit eveniet ea,
-          vero tempore, nostrum est. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Maxime velit vel sed praesentium, placeat ducimus
-          vero facilis eligendi assumenda id! Voluptatem reiciendis natus
-          assumenda architecto eveniet dolorem! Repudiandae, repellendus
-          numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Provident voluptas neque deserunt pariatur, vitae officia incidunt.
-          Officiis, aliquid nulla. Deleniti molestias ipsa fugiat omnis error
-          necessitatibus adipisci facere amet quibusdam!
-        </p>
       </Wrapper>
     </section>
   );
